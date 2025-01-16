a38361d9a7c9acd3bdf7b531ac8c6f7973c22bb5
MrBowis <02alejo20@gmail.com>
2025-01-15 22:13:39 -0500

    Fix errors - Bresenham Line && Incremental Line Algorithm

--- AlgoritmosdeDiscretización/FrmBresenhamLine.cs
+++ AlgoritmosdeDiscretización/FrmBresenhamLine.cs
@@ -41,11 +41,7 @@ namespace AlgoritmosdeDiscretización
 
         private void btnExit_Click(object sender, EventArgs e)
         {
-<<<<<<< HEAD
             this.Hide();
-=======
-            this.Close();
->>>>>>> f5630672465b05e574c2326415810fdbe58b2a62
         }
     }
 }
